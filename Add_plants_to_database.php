28bb21ce39e46e6a9862cab9a6c0dc841e8b81d4
juliencongras <julien.congras@gmail.com>
2026-07-16 10:25:27 +0200

    Added add plants to database functions.

---
+++ Add_plants_to_database.php
@@ -0,0 +1,8 @@
+<form action="add_plant.php" method="post">
+    <p>Nom : <input type="text" name="plant_name"></p>
+    <p>Espece : <input type="text" name="species_name"></p>
+    <p>Date d'achat : <input type="date" name="date_of_purchase" id=""></p>
+    <p>Image : <input type="file" name="plant_image" id=""></p>
+    <p>Quantitée d'eau : <input type="number" name="water_amount" id=""></p>
+    <p>Fréquence d'arrosage : <input type="number" name="watering_frequency" id=""></p>
+</form>
---
+++ add_plant.php
@@ -0,0 +1,33 @@
+<?php
+    $plant_name = $_POST['plant_name'];
+    $species_name = $_POST['species_name'];
+    $date_of_purchase = $_POST['date_of_purchase'];
+    $plant_image = $_POST['plant_image'];
+    $water_amount = $_POST['water_amount'];
+    $watering_frequency = $_POST['watering_frequency'];
+
+    include('pdo.php');
+
+    if($_FILES['plant_image']['size'] != 0){
+        $file_nameimg = $_FILES['plant_image']['name'];
+        $file_tmpimg = $_FILES['plant_image']['tmp_name'];
+        $file=pathinfo($file_nameimg);
+        $i = 1;
+        $filename=$file['filename'];
+
+        while(file_exists("images_plantes/".$filename.".".$file['extension'])){
+            $filename=$file['filename']." ($i)";
+            $i++;
+        }
+
+        $plant_image = "images_plantes/".$filename.".".$file['extension'];
+        move_uploaded_file($file_tmpimg,"images_plantes/".$filename.".".$file['extension']);
+    }
+
+    $req = $pdo->prepare('INSERT INTO Plantes (Plante_nom, Plante_espece, Date_achat, Plante_image, Quantite_eau, 
+    Frequence_arosage) VALUES (?,?,?,?,?,?);');
+    $req->execute([$plant_name, $species_name, $date_of_purchase, $plant_image, $water_amount, $watering_frequency]);
+
+    //replace 'index.php' with the name of the homepage file
+    header('location: index.php');
+?>
